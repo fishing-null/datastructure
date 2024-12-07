ea21a84c63a51ba081f93c0f729128d25a6e1dd0
fishing-null <1134979484@qq.com>
2024-12-07 09:10:53 +0800

    seqlist listElem & insertElem & deleteElem

--- LinerList/seqlist.c
+++ LinerList/seqlist.c
@@ -31,12 +31,53 @@ int appenElem(Seqlist *L,ElementType element){
     L -> length++;
     return 1;
 }
+void listElem(Seqlist *L){
+    for(int i = 0;i < L -> length;i++){
+        printf("%d\t",L->data[i]);
+    }
+    printf("\n");
+}
+void insertElem(Seqlist *L,int pos,ElementType element){
+    //insert an element in seqlist
+    
+    //check if the pos is legal
+    if(pos >= L -> length || pos < 0){
+        printf("Illegal index in seqlist\n");
+        return;
+    }
+    for(int i = L -> length;i >= pos; i--){
+        //move the element behind
+        L -> data[i] = L -> data[i-1];   
+    }
+    L -> data[pos] = element;
+    L -> length += 1;
+}
+void deleteElem(Seqlist* L,int index){
+    if(index >= L -> length || index < 0){
+        printf("Illegal index in seqlist\n");
+        return;
+    }
+    ElementType e = L->data[index];
+    for(int i = index;i < L -> length - 1;i++){
+        L -> data[i] = L -> data[i+1];
+    }
+    L -> length -= 1;
+}
 int main(int argc,char const *rgv[]){
     Seqlist list;
     initList(&list);
     printf("init the seqlist,the length of the list's %d\n",list.length);
     printf("the seqlist use %zu bytes memory\n",sizeof(list));
     appenElem(&list, 567);
+    appenElem(&list, 67);
+    appenElem(&list, 56);
+    appenElem(&list, 565);
     printf("after append one element, the length of the list's %d\n",list.length);
+    listElem(&list);
+    printf("insert 53 in index 2\n");
+    insertElem(&list,3,53);
+    listElem(&list);
+    deleteElem(&list, 2);
+    listElem(&list);
     return 0;
 }
