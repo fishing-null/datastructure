519690b0b05db6021701d8bdf0c4baaebac87f99
fishing-null <1134979484@qq.com>
2024-12-07 09:27:51 +0800

    seqlist findelement

--- LinerList/seqlist.c
+++ LinerList/seqlist.c
@@ -63,9 +63,19 @@ void deleteElem(Seqlist* L,int index){
     }
     L -> length -= 1;
 }
+int findElem(Seqlist *L,ElementType e){
+    for(int i = 0;i < L -> length;i++){
+        if(L -> data[i] == e){
+            printf("Seqlist contains element in %d index\n",i);
+            return 1; 
+        }
+    }
+    return 0;
+}
 int main(int argc,char const *rgv[]){
     Seqlist list;
     initList(&list);
+
     printf("init the seqlist,the length of the list's %d\n",list.length);
     printf("the seqlist use %zu bytes memory\n",sizeof(list));
     appenElem(&list, 567);
@@ -73,11 +83,16 @@ int main(int argc,char const *rgv[]){
     appenElem(&list, 56);
     appenElem(&list, 565);
     printf("after append one element, the length of the list's %d\n",list.length);
+
+
     listElem(&list);
     printf("insert 53 in index 2\n");
     insertElem(&list,3,53);
     listElem(&list);
     deleteElem(&list, 2);
     listElem(&list);
+
+    int n = findElem(&list,67);
+    printf("%d\n",n);
     return 0;
 }
