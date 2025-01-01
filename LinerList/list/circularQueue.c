29aa692739b6d82c0abab8a46755a098fcd59e13
zbj <1134979484@qq.com>
2025-01-01 09:36:29 +0800

    循环队列

---
+++ LinerList/list/circularQueue.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef int ElementType;
+
+typedef struct Node{
+    ElementType data;
+    struct Node *next;
+}Node;
+
+typedef struct Queue{
+    Node *front;
+    Node *rear;
+}Queue;
+
+Queue* init(){
+    Queue *q = (Queue *)malloc(sizeof(Queue));
+    Node *head = (Node *)malloc(sizeof(Node));
+    head -> next = NULL;
+    q -> front = head;
+    q -> rear = head;
+    return q;
+}
+
+void eQueue(Queue *q,ElementType e){
+    // 通过尾插的方式入队
+    Node *cur = (Node *)malloc(sizeof(Node));
+    cur -> data = e;
+    cur -> next = NULL;
+    q -> rear -> next = cur;
+    q -> rear = cur;
+}
+
+void dQueue(Queue *q){
+    // 通过头删的方式出队
+    Node *cur = q -> front -> next;
+    // 让头节点指向下一个节点
+    q -> front -> next = cur -> next;
+    // 如果出队的是最后一个节点，需要更新rear
+    if(cur == q -> rear){
+        q -> rear = q -> front;
+    }
+    // 从内存中释放cur
+    free(cur);
+}
+
+void listQueue(Queue *q){
+    Node *cur = q -> front -> next;
+    while(cur != q -> rear){
+        printf("%d -> ",cur -> data);
+        cur = cur -> next;
+    }
+    printf("%d -> NULL\n",cur -> data);
+}
+
+int isEmpty(Queue *q){
+    return q -> front == q -> rear;
+}
+
+ElementType peek(Queue *q){
+    if(isEmpty(q)){
+        printf("The queue is empty\n");
+        return -1;
+    }
+    return q -> front -> next -> data;
+}
+
+int main(int argc,char const *argv[]){
+    Queue *q = init();
+    eQueue(q,10);
+    eQueue(q,20);
+    eQueue(q,30);
+    eQueue(q,40);
+    eQueue(q,50);
+    listQueue(q);
+    dQueue(q);
+    listQueue(q);
+    printf("peek: %d\n",peek(q));
+    return 1;
+}
